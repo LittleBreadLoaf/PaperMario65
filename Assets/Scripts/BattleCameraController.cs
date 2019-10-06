e9a33e15381f5511ac10d3ca16cd4b454ca0c6ed
Littlebreadloaf <LittleBreadLoaf@Andrews-MacBook-Pro-2.local>
2019-10-05 22:05:12 -0700

    Added battle mario and camera, nothing happens new though

---
+++ Assets/Scripts/BattleCameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleCameraController : MonoBehaviour
+{
+    private Camera cam;
+    private CharacterController mario;
+    private Vector3 initialCamDistance;
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        mario = GameObject.Find("BattleMario").GetComponent<CharacterController>();
+        initialCamDistance = mario.transform.position - cam.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenpos = cam.WorldToScreenPoint(mario.transform.position);
+
+        if (mario.transform.position.y < MarioController.currentFloorPos)
+        {
+            cam.transform.position = Vector3.MoveTowards(cam.transform.position, mario.transform.position - initialCamDistance, Time.deltaTime * mario.velocity.magnitude);
+        }
+        else
+        {
+            Vector3 newLoc = Vector3.MoveTowards(cam.transform.position, mario.transform.position - initialCamDistance, Time.deltaTime * MarioController.speed);
+            cam.transform.position = new Vector3(newLoc.x, cam.transform.position.y, newLoc.z);
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/BattleMarioController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BattleMarioController : MonoBehaviour
+{
+
+    private BoxCollider collisionBox;
+    private Animator animator;
+    private SpriteRenderer renderer;
+    public static float speed = 2.0F;
+    public static float jumpSpeed = 5.0F;
+    public static float gravity = 20.0F;
+    public static float currentFloorPos = 0.275F;
+    private bool isRunning = false;
+    private bool updateFlip = false;
+    private Vector3 moveDirection = Vector3.zero;
+    private float yVelocity = 0;
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        renderer = GetComponent<SpriteRenderer>();
+        collisionBox = GetComponent<BoxCollider>();
+
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            SceneManager.LoadScene("Debug_Map");
+    }
+
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //TODO: Change this to be the "Enemy" superclass, pass in params for battle.
+        //Maybe move this logic into that enemy superclass
+        if (hit.gameObject.name.Equals("Goomba"))
+        {
+            SceneManager.LoadScene("Debug_Fight_Scene");
+        }
+    }
+}
